817b05eb4dbe78b3b7503dc851ebb604a20099ae
Vinh Nguyen <nguyenv2@outlook.com>
2020-04-29 10:43:35 -0700

    initial commit

---
+++ CS Fundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
